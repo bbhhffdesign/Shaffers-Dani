510f01e1861bf30a26d78623acef4d0740b848fd
bauhoffer mariano <bauhoffermariano@gmail.com>
2025-04-29 17:23:50 -0300

    sa

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Background } from "./Background";
-// import Button from "./Button";
+import daniHero from "../assets/svg/dani-2.webp";
+import daniHeroGlobo from "../assets/svg/dani-globo.webp";
 
-function MainPage({currentSection, isLeaving}) {
+function MainPage({ currentSection, isLeaving, scrollDirection }) {
   const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    // Salida hacia abajo desde Hero
+    if (isLeaving && scrollDirection === "down" && currentSection === 0) {
+      setAnimate(true);
+    }
+
+    // Regreso hacia arriba a Hero
+    if (!isLeaving && scrollDirection === "up" && currentSection === 1) {
+      setAnimate(false);
+    }
+  }, [isLeaving, scrollDirection, currentSection]);
 
   return (
     <section className="section hero">
@@ -15,13 +28,25 @@ function MainPage({currentSection, isLeaving}) {
           </div>
         </div>
 
-        <div className={"dani-container"}>
-          <div className="dani-container dani-globo"></div>
+        {/* <div className="dani-container "> */}
+        <div className={`dani-container ${animate ? "animate" : ""}`}>
+          <img
+            src={daniHero}
+            className="dani-hero"
+            alt="Imagen Dani"
+            width="360"
+            height="380"
+            fetchpriority="high"
+          />
+          <div className="dani-container dani-globo">
+            <img src={daniHeroGlobo} alt="" loading="eager" />
+          </div>
         </div>
 
         <div className="button-container"></div>
       </div>
-      <Background animate={animate} isDouble={true}/>
+
+      <Background animate={animate} isDouble={true} />
     </section>
   );
 }
